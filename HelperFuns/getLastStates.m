baf85901377f7dad93ff6fb1a582b3b29681bf9d
confidence_task <12201664+Benorli@users.noreply.github.com>
2021-03-25 14:18:16 +0100

    Add get last states

---
+++ HelperFuns/getLastStates.m
@@ -0,0 +1,43 @@
+function [lastStates] = getLastStates(SessionData, trials)
+% GET_LAST_STATES
+% Return the last Bpod state of a selection of trials
+% 
+%   SYNTAX
+%   [last_states] = get_last_states(SessionData, trials)
+%   
+%   DESCRIPTION
+%   [last_states] = get_last_states(SessionData, trials) takes a
+%   SessionData struct, as output from Bpod and a double trials, 
+%   containing a vector of trial numbers to include. Returns last_states, 
+%   a cell array giving the name of the last occuring state per trial. 
+
+validateattributes(SessionData, {'struct'}, {})
+validateattributes(trials, {'numeric'}, {})
+
+lastStates = arrayfun(@(x) getLastState(SessionData, x), trials);
+
+end
+
+
+function [lastState] = getLastState(SessionData, trial)
+% GET_LAST_STATE
+% Return the last Bpod state of a specific trial
+% 
+%   SYNTAX
+%   [last_states] = get_last_state(SessionData, trial)
+%   
+%   DESCRIPTION
+%   [last_state] = get_last_state(SessionData, trial) takes a
+%   SessionData struct, as output from Bpod and an integer trial, 
+%   containing a trial numbers to include. Returns last_states, 
+%   a cell array giving the name of the last occuring state in trial. 
+
+validateattributes(SessionData, {'struct'}, {})
+validateattributes(trials, {'numeric'}, {'size', [1,1]})
+
+[~, maxIdx] = max(structfun(@(x) max(x, [], 'all'),...
+    SessionData.RawEvents.Trial{trial}.States));
+allFieldnames = fieldnames(SessionData.RawEvents.Trial{trial}.States);
+lastState = allFieldnames(maxIdx);
+
+end
