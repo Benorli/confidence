15fa58b92f8a1c847aff09581aa2afef92ee437e
confidence_task <12201664+Benorli@users.noreply.github.com>
2021-03-25 14:15:53 +0100

    Add IO functions, loadBpodSpike2, trials2table, and first Conversion cell array

---
+++ IO/loadBpodSpike2.m
@@ -0,0 +1,59 @@
+function [SessionData, spikes, spike2TrialStart] = loadBpodSpike2(name)
+% LOADBPODSPIKE2(name)
+%
+%   [SessionData, spikes, spike2TrialStart] = LOADBPODSPIKE2(name)
+%       takes a string name, with the name of the animal. It then
+%       simplifies loading of the recorded SessionData struct, spikes, a 
+%       vector of spike times (in Spike2 time) and spike2TrialStart, a
+%       vector of trialStartTimes (in Spike2 time). Uses ui selection.
+
+validateattributes(name, {'char'}, {})
+
+load(['C:\Users\Ben\Documents\_work_bnathanson\Summary_Analysis\',...
+    'Scripts\confidence\pathStruct'], 'pathStruct');
+
+startDirectory = cd;
+
+% Choose Protocol based on experimentor
+if contains(name, 'BN')
+   sessionLocation = pathStruct.ExperimentorsFromBase.BN;
+elseif contains(name, 'ML')
+   sessionLocation = pathStruct.ExperimentorsFromBase.ML;
+else
+    error('New experimentor, the file location is unknown')
+end
+
+% load Bpod SessionData
+cd([pathStruct.BaseAnimalFolder, name, sessionLocation])
+[SessionDataFile] = uigetfile('.mat', 'Choose SessionData file');
+SessionData = load(SessionDataFile);
+SessionData = SessionData.SessionData;
+
+cd([pathStruct.BaseAnimalFolder, name, '\MATLAB\Spike2Matlab'])
+[spike2Files] = uigetfile('.mat', 'Choose Spike2 files', 'MultiSelect',...
+    'on');
+
+% load Spike2 spikes and trial starts
+assert(numel(spike2Files) == 2, ['Choose two files, one containing spikes,'...
+    'the othe containing trial start times, these are exports from Spike2'])
+
+if contains(spike2Files{1}, 'spike')
+    spikesFile = spike2Files{1};
+    spike2TrialStartFile = spike2Files{2};
+else
+    spike2TrialStartFile = spike2Files{1};
+    spikesFile = spike2Files{2};
+end
+
+% load and extract times from structs, defining the names for output
+spikes = load(spikesFile);
+spikes = spikes.(subsref(fieldnames(spikes), substruct('{}',{1}))...
+    ).times(:);
+
+spike2TrialStart = load(spike2TrialStartFile);
+spike2TrialStart = spike2TrialStart.(subsref(fieldnames(...
+    spike2TrialStart) ,substruct('{}',{1}))).times(:);
+
+cd(startDirectory)
+
+end
---
+++ IO/trials2table.m
@@ -0,0 +1,101 @@
+function [T, varargout] = trials2table(varargin)
+% TRIALS2TABLE
+% Moves trial data into a table, with each row as trial
+%
+% SYNTAX
+% T = trials2table() opens up a uigui, the user is prompted to select a 
+%       data file, which should be a struct. The user is then prometed to 
+%       select a conversion file, which is a cell array containing 3
+%       columns. Column 1 for the new name of the variable, column 2 for
+%       the old name and column 3 for an intermediate function. A table is
+%       then returned with each variable as a column vector.
+%
+% NAME VALUE OPTIONAL IN:
+% dataFile: Either a file location for a data file which must be a struct,
+%       or a variable containing the data struct.
+% conversionFile: Either a file location for a conversion file which must 
+%       be a cell array with 3 columns, or a variable containing the 
+%       cell array.
+
+% set defaults
+defaultDatafile = false;
+defaultConversionFile = false;
+
+isFileLoc = @(x) ischar(x) || isstring(x) || iscellstr(x);
+validConversionArrayOrFile = @(x) isFileLoc(x) ||...
+    iscell(x) && size(x, 2) == 3;
+validStructOrFile = @(x) isFileLoc(x) ||...
+    isstruct(x);
+
+p = inputParser;
+addParameter(p, 'dataFile', defaultDatafile, validStructOrFile);
+addParameter(p, 'conversionFile', defaultConversionFile,...
+    validConversionArrayOrFile);
+parse(p, varargin{:});
+
+dataFile = p.Results.dataFile;
+conversionFile = p.Results.conversionFile;
+clear p
+
+start_directory = cd;
+
+% if no input, select files with gui
+if ~dataFile
+    [filename, pathname] = uigetfile('*.mat',...
+        'Choose Session to Analyse', 'multiselect', 'off');
+    % stop if user cancels
+    if isequal(filename, 0)
+        disp('User selected cancel')
+        return;
+    end
+    dataFile = [pathname, filename];
+end
+
+if isFileLoc(dataFile) % if input is file location
+    data = load(dataFile);
+    data = data.(subsref(fieldnames(data), substruct('{}',{1}))); % unwrap
+    assert(isstruct(data), 'The data file for trials must be a struct')
+else % if input is data struct
+    data = dataFile;
+end
+
+% if no input, select files with gui
+if ~conversionFile
+    [filename, pathname] = uigetfile('*.mat',...
+        'Choose Conversion Cell Array', 'multiselect', 'off');
+    % stop if user cancels
+    if isequal(filename, 0)
+        disp('User selected cancel')
+        return;
+    end
+    conversionFile = [pathname, filename];
+end
+
+if isFileLoc(conversionFile) % if input is file location
+    conversion = load(conversionFile);
+    conversion = conversion.(subsref(fieldnames(conversion),...
+        substruct('{}',{1}))); % unwrap
+    assert(validConversionArrayOrFile(conversion), ['Conversion array must ',...
+        'be a cell array with 3 columns'])
+else % if input is conversion cell array
+    conversion = conversionFile;
+end
+
+clear conversionFile dataFile
+
+% move data into data table
+T = table((1:data.nTrials)', 'VariableNames', {'trialNumber'});
+
+for i = 1: size(conversion, 1)
+    newName = conversion{i, 1};
+    oldName = conversion{i, 2};
+    convFun = conversion{i, 3};
+    T.(newName) = convFun(data.(oldName));
+end
+
+varargout{1} = conversion;
+varargout{1} = data;
+
+cd(start_directory)
+    
+end
