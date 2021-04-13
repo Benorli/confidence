4ce199fc9bec7f2efb31e779432c77a64c945936
confidence_task <12201664+Benorli@users.noreply.github.com>
2021-04-13 15:43:49 +0200

    Add helperfuns to extract clicks based on side

---
+++ HelperFuns/clicksOccuredLeft.m
@@ -0,0 +1,25 @@
+function [leftClicks] = clicksOccuredLeft(fastClicks, slowClicks, samplingDuration, trialType)
+% return the click times of clicks which occured on the left side
+
+validateattributes(fastClicks, {'cell'}, {'nonempty'});
+validateattributes(slowClicks, {'cell'}, {'nonempty'});
+validateattributes(samplingDuration, {'double'}, {'nonempty'});
+validateattributes(trialType, {'double'}, {'nonempty'});
+
+
+leftClicks = cell(size(fastClicks)); 
+
+for i = 1:length(fastClicks)
+    
+    switch trialType(i)
+        case 1
+            tempClicks = fastClicks{i};
+            tempClicks(tempClicks>samplingDuration(i)) = [];
+            leftClicks{i} = tempClicks;
+        case 2
+            tempClicks = slowClicks{i};
+            tempClicks(tempClicks>samplingDuration(i)) = [];
+            leftClicks{i} = tempClicks;
+    end
+    
+end
---
+++ HelperFuns/clicksOccuredRight.m
@@ -0,0 +1,25 @@
+function [rightClicks] = clicksOccuredRight(fastClicks, slowClicks, samplingDuration, trialType)
+% return the click times of clicks which occured on the right side
+
+validateattributes(fastClicks, {'cell'}, {'nonempty'});
+validateattributes(slowClicks, {'cell'}, {'nonempty'});
+validateattributes(samplingDuration, {'double'}, {'nonempty'});
+validateattributes(trialType, {'double'}, {'nonempty'});
+
+
+rightClicks = cell(size(fastClicks)); 
+
+for i = 1:length(fastClicks)
+    
+    switch trialType(i)
+        case 2
+            tempClicks = fastClicks{i};
+            tempClicks(tempClicks>samplingDuration(i)) = [];
+            rightClicks{i} = tempClicks;
+        case 1
+            tempClicks = slowClicks{i};
+            tempClicks(tempClicks>samplingDuration(i)) = [];
+            rightClicks{i} = tempClicks;
+    end
+    
+end
