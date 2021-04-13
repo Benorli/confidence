6ea8a35bad833fa3dd14757f97860d5b664c5717
PaulMAnderson <37668570+PaulMAnderson@users.noreply.github.com>
2021-04-13 13:08:14 +0200

    Update trialExplorer.m

--- HelperFuns/trialExplorer.m
+++ HelperFuns/trialExplorer.m
@@ -12,8 +12,11 @@ assert(trialNum <= sessionData.nTrials,'Trial Number too high');
 
 %% 
 
-% Was the trial completed (completed = finished?)
-trialInfo.Completed = sessionData.CompletedTrial(trialNum);
+% Which side was the trial evidence (supposed to be) on?
+trialInfo.EvidenceSideBpod = sessionData.TrialTypes;
+
+% How long did the animal sample the evidence?
+trialInfo.SamplingDuration = sessionData.SamplingDuration;
 
 % Was the trial a catch trial
 trialInfo.CatchTrial = sessionData.CatchTrial(trialNum);
@@ -30,5 +33,83 @@ trialInfo.CorrectSide = sessionData.CorrectSide(trialNum);
 % What was the chosen direction (chosen direction = actual choice?)
 switch sessionData.ChosenDirection(trialNum)
     case 1
-        
-trialInfo.ChosenDirection = sessionData.ChosenDirection(trialNum);
+        trialInfo.ChosenDirection = categorical({'Correct'});
+    case 2
+        trialInfo.ChosenDirection = categorical({'Incorrect'});
+    case 3
+        trialInfo.ChosenDirection = categorical({'No Choice'});
+end
+
+% Which side actually had the most clicks 
+switch sessionData.MostClickSide(trialNum) % if they are even this is random
+    case 1
+        trialInfo.MostclickSide = categorical({'Left'});
+    case 2
+        trialInfo.MostclickSide = categorical({'Right'});
+end
+
+% Which side did the animal choose given the evidence heard?
+trialInfo.ChoiceGivenClick = sessionData.ChoiceGivenClick(trialNum);
+
+% Was the trial completed (completed = Punish|WaitforReward occurs)
+trialInfo.Completed = sessionData.CompletedTrial(trialNum);
+
+% Was it a completed catch trial (completed trial and a catch trial)
+trialInfo.CompletedCatchTrial = sessionData.CompletedCatchTrial;
+
+% What type of completed catch trial was it?
+% Type 1 = CompletedCatchTrial & Correct Choice Given Clicks
+trialInfo.CorrectCatchType1 = sessionData.CorrectCatchTrial_type1(trialNum);
+% Type 2 = Wait for Reward Start & ~Reward & ~CompletedCatch
+% Maybe this is when the choice given clicks doesn't match the programmed side?
+trialInfo.CorrectCatchType2 = sessionData.CorrectCatchTrial_type2(trialNum);
+
+% How long did the animal wait for?
+trialInfo.AnimalWaited = sessionData.WaitingTime(trialNum);
+
+% Was the catch trial correct? correctCatchTrial =
+% correctCatch1|correctCatch2 if ConfidenceReport == 1 in GUI...
+trialInfo.CorrectCatchTrial = sessionData.CorrectCatchTrial(trialNum);
+
+% Was the catch trial incorrect? incorrectCatchTrial =
+% completedCatch & ~choicegivenclick
+trialInfo.IncorrectCatchTrial = sessionData.IncorrectCatchTrial(trialNum);
+
+% How hard was the choice? RatioDiscri = log10(NRightClick./NLeftClick)
+trialInfo.StimulusDifficulty = sessionData.RatioDiscri(trialNum);
+
+% Was the trial rewarded? Trial is rewarded if waitforReward and Reward
+% states both occur
+trialInfo.RewardedTrial = sessionData.RewardedTrial(trialNum);
+
+% Correct sides = choice give click is correct and side is left/right
+trialInfo.CorrectLeft = sessionData.CorrectLeft(trialNum);
+trialInfo.CorrectRight = sessionData.CorrectRight(trialNum);
+
+% Incorrect sides = choice give click is incorrect and side is left/right
+trialInfo.ErrorLeft = sessionData.ErrorLeft(trialNum);
+trialInfo.ErrorRight = sessionData.ErrorRight(trialNum);
+
+
+
+
+
+
+
+
+
+% % Get correct WaitingTime Trials
+% BpodSystem.Data.CorrectWaitingTimeTrials(currentTrial)=BpodSystem.Data.CorrectCatchTrial(currentTrial)==1 & ...
+%     BpodSystem.Data.WaitingTime(currentTrial)>BpodSystem.Data.WTlimitLow & ...
+%     BpodSystem.Data.WaitingTime(currentTrial)<BpodSystem.Data.WTlimitHigh;
+
+% Get incorrect WaitingTime Trials
+% BpodSystem.Data.InCorrectWaitingTimeTrials(currentTrial)= ...
+%     (BpodSystem.Data.IncorrectCatchTrial(currentTrial)==1 |  ...
+%     ((BpodSystem.Data.PunishedTrial(currentTrial)==1 &  ...
+%     BpodSystem.Data.CompletedTrial(currentTrial)==1) &  ...
+%     BpodSystem.Data.ChoiceGivenClick(currentTrial)==0)) & ...
+%     BpodSystem.Data.WaitingTime(currentTrial)>BpodSystem.Data.WTlimitLow & ...
+%     BpodSystem.Data.WaitingTime(currentTrial)<BpodSystem.Data.WTlimitHigh;
+
+
